e30e8192c4214c7f385bf6e66ba4cf3bef04cf4f
carlosoliveira397 <carlosd.oliveira397@gmail.com>
2018-08-16 03:33:02 +0900

    botoes com funcao

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -1,9 +1,36 @@
 // This is a JavaScript file
- $(document).on("click", "#somar", function(){
+    $(document).on("click", "#somar", function(){
      var valor1 = parseInt($("#valor1").val());
      var valor2 = parseInt($("#valor2").val());
      
      var result = valor1 + valor2;
+     
+     $("#result").val(result);
+     });
+
+    $(document).on("click", "#sub", function(){
+     var valor1 = parseInt($("#valor1").val());
+     var valor2 = parseInt($("#valor2").val());
+     
+     var result = valor1 - valor2;
+     
+     $("#result").val(result);
+     });
+     
+    $(document).on("click", "#divd", function(){
+     var valor1 = parseInt($("#valor1").val());
+     var valor2 = parseInt($("#valor2").val());
+     
+     var result = valor1 / valor2;
+     
+     $("#result").val(result);
+     });
+     
+    $(document).on("click", "#mult", function(){
+     var valor1 = parseInt($("#valor1").val());
+     var valor2 = parseInt($("#valor2").val());
+     
+     var result = valor1 * valor2;
      
      $("#result").val(result);
      });
